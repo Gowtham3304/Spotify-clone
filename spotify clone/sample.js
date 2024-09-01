cd0c784399dc0502f554f9339548dd3a2101536f
Gowtham P <107994341+Gowtham3304@users.noreply.github.com>
2024-09-01 15:55:04 +0530

    Add files via upload

---
+++ spotify clone/sample.js
@@ -0,0 +1,13 @@
+class person
+{
+    constructor(name){
+        cons var =name;
+    }
+    greet()
+    {
+        console.log("hello this is my "+var);
+    }
+}
+const obj=person
+const db=new obj("Puvijay")
+db.greet()
